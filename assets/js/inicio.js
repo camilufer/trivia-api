11275d9224322a09aa5e387bcddeca614c51388c
camilufer <camilu.fer21@gmail.com>
2018-02-22 17:20:44 -0300

    firebase e inicio

---
+++ assets/js/inicio.js
@@ -0,0 +1,55 @@
+var provider = new firebase.auth.FacebookAuthProvider();
+provider.addScope('user_birthday');
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+
+firebase.auth().signInWithRedirect(provider);
+
+firebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential) {
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    var token = result.credential.accessToken;
+    // ...
+  }
+  // The signed-in user info.
+  var user = result.user;
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+
+/*
+<script>
+    var ref = new Firebase("https://canvasfirebase.firebaseio.com/");
+
+    document.getElementById("loginfacebook").addEventListener("click", function(){
+      ref.authWithAuthpopup("facebook", function(error, authData){
+        if (error) {
+          console.log("Error", error.message);
+        } else {
+          console.log("ok", authData);
+        }
+      });
+    });
+  </script>
+  */
