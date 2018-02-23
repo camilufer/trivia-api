cc7490a28ba198daac9a3cb099f9f709d6a2ea1a
camilufer <camilu.fer21@gmail.com>
2018-02-23 00:45:45 -0300

    firebase

--- assets/js/inicio.js
+++ assets/js/inicio.js
@@ -1,55 +1,25 @@
-var provider = new firebase.auth.FacebookAuthProvider();
-provider.addScope('user_birthday');
-firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
+function IngresoFacebook(){
+	if (!firebase.auth().currentUser){
+		var provider = new firebase.auth.FacebookAuthProvider();
+		provider.addScope('public_profile');
+		firebase.auth().singInWithPopup(provider).then (function(result){
+			var token = result.credential.accesstoken;
+			var user = result.user;
+			console.log(user);
 
-firebase.auth().signInWithRedirect(provider);
+		}).catch (function(error) {
+			var errorCode = error.code;
+			var errorMessage = error.Message;
+			var erroremail = error.email;
+			var credential = error.credential;
+			if (errorCode==='auth/account-exist-whith-different-credential'){
+				alert ('Es el mismo usuario')
+			}
+		});
 
-firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
-    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-    var token = result.credential.accessToken;
-    // ...
-  }
-  // The signed-in user info.
-  var user = result.user;
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
+	}else {
+		firebase.auth().singout;
+	}
+}
 
-/*
-<script>
-    var ref = new Firebase("https://canvasfirebase.firebaseio.com/");
-
-    document.getElementById("loginfacebook").addEventListener("click", function(){
-      ref.authWithAuthpopup("facebook", function(error, authData){
-        if (error) {
-          console.log("Error", error.message);
-        } else {
-          console.log("ok", authData);
-        }
-      });
-    });
-  </script>
-  */
+document.getElementById('loginfacebook').addEventListener('click', IngresoFacebook, false);
